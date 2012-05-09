1b62a8bebc94fe9319424bdbf57b5d48cac3b09f
Barry Wardell <barry.wardell@gmail.com>
2012-05-09 18:52:19 +0100

    Switch to quad precision and allow for a time scaling of the field.

--- wave_bh_rk4.cc
+++ wave_bh_rk4.cc
@@ -14,46 +14,89 @@
  *
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-#include <gsl/gsl_math.h>
-#include <gsl/gsl_sf_lambert.h>
+extern "C" {
+#include <quadmath.h>
+}
 
 /* User specified parameters */
 const int    N = 40001;				/* Number of points */
-const long double rstarMax = 4000.0L;		/* Size of grid */
-const long double T = 8000L;				/* Final time */
+const __float128 rstarMax = 4000.0L;		/* Size of grid */
+const __float128 T = 8000L;				/* Final time */
 const int    l = 1;					/* Spherical harmonic mode */
-const long double M = 1.0L;				/* Mass of the black hole */
+const __float128 M = 1.0L;				/* Mass of the black hole */
+const int scaling = 0;
 
 /* Derived parameters */
-const long double h = 2.0L*rstarMax/(N-1); /* Grid spacing */
-const long double k = 0.25L*h;			 /* Minimum time step size */
+const __float128 h = 2.0L*rstarMax/(N-1); /* Grid spacing */
+const __float128 k = 0.25L*h;			 /* Minimum time step size */
 
 /* Calculate the right hand sides */
-void rhs(const long double V[], const long double phi[], const long double rho[], long double phidot[], long double rhodot[])
+void rhs(const __float128 t, const __float128 V[], const __float128 phi[], const __float128 rho[], __float128 phidot[], __float128 rhodot[])
 {
+  __float128 rho_coeff, phi_coeff;
+  __float128 t_scaling_2, t_scaling_1, t_scaling;
+
+  __float128 t3 = t*t*t;
+  __float128 t4 = t3*t;
+  __float128 t5 = t4*t;
+
+  switch(scaling) {
+  case 0:
+    phi_coeff = 0.0L;
+    rho_coeff = 0.0L;
+    break;
+  case 1:
+    phi_coeff = -2.0L / ((1.0L + t) * (1.0L + t));
+    rho_coeff = 2.0L / (1.0L + t);
+    break;
+  case 2:
+    phi_coeff = (2.0L - 6.0L*t*t) / ((1.0L + t) * (1.0L + t));
+    rho_coeff = 4.0L * t / (1.0L + t*t);
+    break;
+  case 5:
+    phi_coeff = -(10.0L*t3)*(3.0L*t5-2.0L) / ((1.0L + t5) * (1.0L + t5));
+    rho_coeff = 10.0L * t4 / (1.0L + t5);
+    break;
+  default:
+    t_scaling_2 = pow(t, scaling-2.0L);
+    t_scaling_1 = t*t_scaling_2;
+    t_scaling   = t*t_scaling_1;
+    phi_coeff = - scaling * t_scaling_2 * (1.0L + t_scaling + scaling * (t_scaling - 1.0L)) /
+        ((1.0L + t_scaling) * (1.0L + t_scaling));
+    rho_coeff = 2.0L * scaling * t_scaling_1 / (1.0L + t_scaling);
+    break;
+  }
+
   /* Calculate interior */
 #pragma omp parallel for
   for(int i=2; i<N-2; i++)
   {
-    long double phi_xx = (-phi[i+2] + 16L*phi[i+1] - 30L*phi[i] + 16L*phi[i-1] - phi[i-2])/(12.0L*h*h);
+    __float128 phi_xx = (-phi[i+2] + 16L*phi[i+1] - 30L*phi[i] + 16L*phi[i-1] - phi[i-2])/(12.0L*h*h);
     phidot[i] = rho[i];
-    rhodot[i]  = phi_xx - V[i]*phi[i];
+    rhodot[i]  = phi_xx - phi[i] * V[i]
+      + rho[i] * rho_coeff
+      + phi[i] * phi_coeff;
   }
 
   /* Second last points */
   {
     int i=1;
-    long double phi_xx = (phi[i+1] - 2L*phi[i] + phi[i-1])/(h*h);
+    __float128 phi_xx = (phi[i+1] - 2L*phi[i] + phi[i-1])/(h*h);
     phidot[i] = rho[i];
-    rhodot[i]  = phi_xx - V[i]*phi[i];
+    rhodot[i]  = phi_xx - phi[i] * V[i]
+      + rho[i] * rho_coeff
+      + phi[i] * phi_coeff;
   }
 
   {
     int i=N-2;
-    long double phi_xx = (phi[i+1] - 2L*phi[i] + phi[i-1])/(h*h);
+    __float128 phi_xx = (phi[i+1] - 2L*phi[i] + phi[i-1])/(h*h);
     phidot[i] = rho[i];
-    rhodot[i]  = phi_xx - V[i]*phi[i];
+    rhodot[i]  = phi_xx - phi[i] * V[i]
+      + rho[i] * rho_coeff
+      + phi[i] * phi_coeff;
   }
   
   /* Horizon boundary */
@@ -70,67 +113,77 @@ void rhs(const long double V[], const long double phi[], const long double rho[]
 }
 
 /* Output values of fields */
-void output(long double t, long double rstar[], long double phi[], long double rho[]) {
+void output(__float128 t, __float128 rstar[], __float128 phi[], __float128 rho[]) {
 //   for (int j = 0; j < N; j++) {
 //     printf("%.19f\t", phi[j]);
 //   }
   int i = (N-1)/2;
-  printf("%.19Lg\t%.19Lg\t%.19Lg\t%.19Lg\t", t, rstar[i], phi[i], rho[i]);
+  char* y = new char[1000];
+  quadmath_snprintf(y, 1000, "%.19Qg", t);
+  printf("%s\t", y);
+  quadmath_snprintf(y, 1000, "%.19Qg", rstar[i]);
+  printf("%s\t", y);
+  quadmath_snprintf(y, 1000, "%.19Qg", phi[i]);
+  printf("%s\t", y);
+  quadmath_snprintf(y, 1000, "%.19Qg", rho[i]);
+  printf("%s", y);
   printf("\n");
 }
 
 int main()
 {
   /* Setup grid */
-  long double rstar0 = 12.7725887222397812376689284858327062723020005374410210164827L;
-  long double *V     = new long double[N];
-  long double *rstar = new long double[N];
+  __float128 rstar0 = 12.7725887222397812376689284858327062723020005374410210164827L;
+  __float128 *V     = new __float128[N];
+  __float128 *rstar = new __float128[N];
 
   FILE * potential;
   potential = fopen ("potential.dat","r");
   for (int j = 0; j < N; j++) {
     rstar[j] = rstar0-rstarMax + j * h;
+    char tmp[1000];
     
     /* Pre-compute the potential */
-    /*long double rm2M = 2.0L*M*gsl_sf_lambert_W0(exp(-1.0L+rstar[j]/(2.0L*M)));
-    long double r = rm2M + 2.0L*M;
+    /*__float128 rm2M = 2.0L*M*gsl_sf_lambert_W0(exp(-1.0L+rstar[j]/(2.0L*M)));
+    __float128 r = rm2M + 2.0L*M;
 	V[j] = rm2M*(l*(l+1)*r + 2.0L*M)/(r*r*r*r);*/
-	fscanf(potential, "%Lg\n", &V[j]);
+	fscanf(potential, "%s\n", tmp);
+	V[j] = strtoflt128 (tmp, NULL);
   }
   fclose (potential);
 
-  long double *phi = new long double[N];
-  long double *rho = new long double[N];
+  __float128 *phi = new __float128[N];
+  __float128 *rho = new __float128[N];
 
   /* Initial Conditions: a gaussian about r=10 */
   for (int j = 0; j < N; j++) {
-    phi[j] = exp(-(rstar[j]-rstar0)*(rstar[j]-rstar0)/2.0L);
+    phi[j] = expq(-(rstar[j]-rstar0)*(rstar[j]-rstar0)/2.0L);
     rho[j] = -(rstar[j]-rstar0)*phi[j];
   }
 
   /* Allocate scratch space for integration */
-  long double *phidot = new long double[N];
-  long double *rhodot = new long double[N];
-  long double *phi_tmp = new long double[N];
-  long double *rho_tmp = new long double[N];
+  __float128 *phidot = new __float128[N];
+  __float128 *rhodot = new __float128[N];
+  __float128 *phi_tmp = new __float128[N];
+  __float128 *rho_tmp = new __float128[N];
   
-  long double *k1_phi = new long double[N];
-  long double *k1_rho = new long double[N];
-  long double *k2_phi = new long double[N];
-  long double *k2_rho = new long double[N];
-  long double *k3_phi = new long double[N];
-  long double *k3_rho = new long double[N];
-  long double *k4_phi = new long double[N];
-  long double *k4_rho = new long double[N];
+  __float128 *k1_phi = new __float128[N];
+  __float128 *k1_rho = new __float128[N];
+  __float128 *k2_phi = new __float128[N];
+  __float128 *k2_rho = new __float128[N];
+  __float128 *k3_phi = new __float128[N];
+  __float128 *k3_rho = new __float128[N];
+  __float128 *k4_phi = new __float128[N];
+  __float128 *k4_rho = new __float128[N];
 
   /* Fourth order Runge-Kutta time integration */
-  for (long double t = 0; t <= T; t+=k)
+  for (__float128 t = 0; t <= T; t+=k)
   {
     /* Output the results */
     output(t, rstar, phi, rho);
 
   	/* Calculate k1 term in RK4 formula */
-  	rhs(V, phi, rho, phidot, rhodot);
+    rhs(t, V, phi, rho, phidot, rhodot);
 
 #pragma omp parallel for
     for (int j = 0; j < N; j++)
@@ -147,7 +200,7 @@ int main()
       rho_tmp[j] = rho[j] + 0.5L*k1_rho[j];
     }
 
-  	rhs(V, phi_tmp, rho_tmp, phidot, rhodot);
+    rhs(t + 0.5L*k, V, phi_tmp, rho_tmp, phidot, rhodot);
 
 #pragma omp parallel for
     for (int j = 0; j < N; j++)
@@ -164,7 +217,7 @@ int main()
       rho_tmp[j] = rho[j] + 0.5L*k2_rho[j];
     }
 
-  	rhs(V, phi_tmp, rho_tmp, phidot, rhodot);
+    rhs(t + 0.5L*k, V, phi_tmp, rho_tmp, phidot, rhodot);
 
 #pragma omp parallel for
     for (int j = 0; j < N; j++)
@@ -181,7 +234,7 @@ int main()
       rho_tmp[j] = rho[j] + k3_rho[j];
     }
 
-  	rhs(V, phi_tmp, rho_tmp, phidot, rhodot);
+    rhs(t + k, V, phi_tmp, rho_tmp, phidot, rhodot);
 
 #pragma omp parallel for
     for (int j = 0; j < N; j++)
